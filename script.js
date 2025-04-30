c7d6e2e459a6ee53117d1c649fcf49b2a1a5a3f9
DhoniTita <dhooni.tita@gmail.com>
2025-04-30 19:56:49 +0700

    Portofolio 1.0

--- script.js
+++ script.js
@@ -9,7 +9,24 @@ function text_animation_1 (){
     setTimeout(text_animation_2,5000)
 }
 setInterval(text_animation_1,10000)
+// TEXT Animation 2
 
+const profesion = document.querySelector('.home_profesion i')
+
+function text_animation2(){
+    profesion.textContent = 'designer'
+    function text_animation2_1(){
+        profesion.textContent = 'editor'
+        function text_animation2_2(){
+            profesion.textContent = 'web_development'
+            
+        }
+        setTimeout(text_animation2_2,1000 * 10)
+    }
+    setTimeout(text_animation2_1,1000 * 10)
+}
+setTimeout(text_animation2,1)
+setInterval(text_animation2,1000 * 30)
 // MENUCLICK
 const menubtn = document.querySelector('.menubtn')
 
@@ -19,5 +36,25 @@ menubtn.addEventListener('click',function menuclick(){
 })
 
 // NAV ACTIVE
-const nav_links = document.querySelectorAll('.menu-list a')
+const main = document.querySelector('main')
+let section = document.querySelectorAll('section')
+let navlikn = document.querySelectorAll('.menulist ul li a')
 
+main.addEventListener('scroll',() => {
+    section.forEach(sec => {
+        let top = main.scrollTop
+        let offset = sec.offsetTop - 150
+        let height = sec.offsetHeight
+        let id = sec.getAttribute('id')
+
+        if(top >= offset && top < offset + height) {
+            navlikn.forEach(links => {
+                links.classList.remove('active')
+                document.querySelector('.menulist ul li a[href*='+ id +']').classList.add('active')
+            })
+        }
+    })
+})
+
+// TEXT PROPERTY
+document.querySelector('.name').textContent = 'i ramadhoni'
